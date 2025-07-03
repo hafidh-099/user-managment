402a7727992f6ce0f252b8b9a96b4628fa928530
hafidh-099 <hafinhoramsey@gmail.com>
2025-07-03 23:43:09 +0300

    store seesion to mysql_db

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const app = express();
 const bodypurse = require("body-parser");
 const session = require("express-session");
 const myuser = require("./routes/users.route");
-const myauth = require("./routes/auth.route")
+const myauth = require("./routes/auth.route");
+const sessioninfo = require("./utils/sessiondata");
+const sessionDB = require("express-mysql-session")(session);
 
 app.use(bodypurse.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+const session_db = new sessionDB(sessioninfo);
+
 app.use(
   session({
     key: "session_id",
     secret: "secret_key",
     resave: false,
     saveUninitialized: false,
+    store: session_db,
   })
 );
 //user route
 app.use("/", myuser);
 app.use("/users", myuser);
 //auth route
-app.use('/',myauth)
+app.use("/", myauth);
 
 app.listen(3000, () => {
   console.log("server is running on http://localhost:3000");
---
+++ utils/sessiondata.js
@@ -0,0 +1,10 @@
+const sessioninfo = {
+  host: "localhost",
+  database: "user_auth",
+  port: 3306,
+  password: "12345678",
+  user: "root",
+  createDatabaseTable: true,
+  connectionLimit: 10,
+};
+module.exports=sessioninfo;
